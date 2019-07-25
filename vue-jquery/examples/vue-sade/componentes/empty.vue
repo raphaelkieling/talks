2cf8b31cd5c8f99bcdbdeff58b374eb0a315fb11
Raphael Kieling <raphael.kieling@egsys.com.br>
2019-07-25 13:25:57 -0300

    Fix something

--- vue-jquery/examples/vue-sade/componentes/empty.vue
+++ vue-jquery/examples/vue-sade/componentes/empty.vue
@@ -1,6 +1,6 @@
 <template functional>
   <div>
-    <img src="https://image.flaticon.com/icons/svg/157/157285.svg" class="sm opacity" />
+    <img src="../assets/empty.svg" class="sm opacity" />
     <p class="text-muted">{{props.message}}</p>
   </div>
 </template>
